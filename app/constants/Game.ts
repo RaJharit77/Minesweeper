9fc98defb327a5567f5915870c72b637ad8dc39c
RaJharit77 <hei.rajo.2@gmail.com>
2025-11-30 22:14:43 +0300

    feat: Add all constants such as color number, grid and cell size and also bombs count

---
+++ app/constants/Game.ts
@@ -0,0 +1,12 @@
+export const GRID_SIZE = 20;
+export const BOMBS_COUNT = 40;
+export const CELL_SIZE = 15;
+
+export const NUMBER_COLORS = {
+    1: "#00A2FF",
+    2: "#00B300",
+    3: "#FFD500",
+    4: "#FF8C00",
+    5: "#FF3B3B",
+    6: "#B80000",
+};
